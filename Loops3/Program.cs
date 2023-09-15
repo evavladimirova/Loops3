7b6b47d7e37a48bbdbff8ba7f1384c7c8a6fb8da
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 21:19:14 +0300

    Add project files.

---
+++ Loops3/Program.cs
@@ -0,0 +1,15 @@
+﻿int n1 = 0;
+int n2 = 1;
+int n3, i, num;
+
+Console.Write("Input number of terms to display:  ");
+num = int.Parse(Console.ReadLine());
+Console.Write(n1 + " " + n2 + " ");
+
+for (i = 2; i < num; ++i)
+{
+    n3 = n1 + n2;
+    Console.Write(n3 + " ");
+    n1 = n2;
+    n2 = n3;
+}
